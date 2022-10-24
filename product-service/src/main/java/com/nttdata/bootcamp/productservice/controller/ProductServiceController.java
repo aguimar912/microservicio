3e8528c82bf636292277dc36c09076a76e2009c8
aguimar912 <91743119+aguimar912@users.noreply.github.com>
2022-10-24 20:19:18 +0200

    Add files via upload

---
+++ product-service/src/main/java/com/nttdata/bootcamp/productservice/controller/ProductServiceController.java
@@ -0,0 +1,18 @@
+package com.nttdata.bootcamp.productservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ProductServiceController {
+
+	@Value("${server.port}")
+	private String port;
+	
+	@GetMapping(path = "/port")
+	public String getPort() {
+		return "El puerto del micro es: " + port;
+	}
+}
